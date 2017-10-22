f28cc9af88d302fb9a8465ef0551c0853a76dcbd
Hideto Obara <hidetobara@gmail.com>
2017-10-22 16:39:02 +0900

    VS2017にアップグレード

---
+++ Web6/DefineSample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web6
+{
+	class DefineSample
+	{
+		public const string CONSUMER = "*";
+		public const string CONSUMER_SECRET = "*";
+		public const string BARA_ACCESS_KEY = "*";
+		public const string BARA_ACCESS_SECRET = "*";
+	}
+}
